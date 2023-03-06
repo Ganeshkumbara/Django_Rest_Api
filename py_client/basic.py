5454350bf148b6e31b600ade367117805928cf33
Ganeshkumbara <ganesh700511@gmail.com>
2023-03-06 19:19:54 +0530

    request and http response basic

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,4 +3,5 @@ import pprint
 endpoint = "https://httpbin.org/anything"
 
 get_res = requests.get(endpoint, json={'query':'tested123'})
-pprint.pprint(get_res.json())
+pprint.pprint(get_res.json()) # Http request
+print(get_res.status_code)
