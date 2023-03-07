8cd285ccaade6fec00d03d31b7c90a8a64f52d93
Ganeshkumbara <ganesh700511@gmail.com>
2023-03-07 20:33:44 +0530

    created 2 apps named api and product, and established database connection with postgresql

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,8 @@
 import requests
 import pprint
-endpoint = 'http://localhost:8080/'
+endpoint = 'http://localhost:8080/api/'
 
-get_res = requests.get(endpoint, json={'query':'tested123'})
-print(get_res.text)
+get_res = requests.get(endpoint, json={'query':'tested123'}, params={'passing':123})
+# print(get_res.text)
+print(get_res.status_code)
+print(get_res.json())
